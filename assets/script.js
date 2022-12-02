085c5834b23f1e8deaed08c29022f80dfc72f8fb
Wir3s <aandrebess@gmail.com>
2022-12-01 20:00:56 -0600

    Array console.log added

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,30 @@
-var buttonEl = document.getElementById("");
+var buttonEl = document.getElementById("ID OF BUTTON");
 
 // Beverage API permissions
 const bevOptions = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "e42bdfecf5mshf3552e7e1249ca6p16c62fjsn997c4e790fc4",
-      "X-RapidAPI-Host": "cocktail-by-api-ninjas.p.rapidapi.com",
-    },
-  };
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e42bdfecf5mshf3552e7e1249ca6p16c62fjsn997c4e790fc4",
+    "X-RapidAPI-Host": "cocktail-by-api-ninjas.p.rapidapi.com",
+  },
+};
 
 // Recipe API permissions
 const recipeOptions = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "cbfed17221msh2efdb0eadae0eefp18de4bjsn3cd21032b10f",
-      "X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com",
-    },
-  };
-
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "cbfed17221msh2efdb0eadae0eefp18de4bjsn3cd21032b10f",
+    "X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com",
+  },
+};
 
 // Search function
 var userSearch = function (event) {
-    event.preventDefault();
-    var searchInput = document.getElementById("FORM INPUT FIELD ID").value;
-    console.log(searchInput);
-    beverageSearch(searchInput);
-  };
-
+  event.preventDefault();
+  var searchInput = document.getElementById("FORM INPUT FIELD ID").value;
+  console.log(searchInput);
+  beverageSearch(searchInput);
+};
 
 // Beverage Fetch code from origin
 
@@ -35,9 +33,9 @@ fetch(
   bevOptions
 )
   .then((response) => response.json())
-  .then((response) => console.log(response))
+  // .then((response) => console.log(response))
+  .then((response) => console.log(response[0].ingredients))
   .catch((err) => console.error(err));
-
 
 // Recipe Fetch code from origin
 
@@ -46,9 +44,7 @@ fetch(
   recipeOptions
 )
   .then((response) => response.json())
-  .then((response) => console.log(response))
+  .then((response) => console.log(response[0].ingredients))
   .catch((err) => console.error(err));
-
-
 
 buttonEl.addEventListener("click", userSearch);
