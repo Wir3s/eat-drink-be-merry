bde9e062bdcf4f3e3db04284b6304d76a013704e
Wir3s <aandrebess@gmail.com>
2022-12-01 19:40:15 -0600

    added fetch calls and listener function

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,54 @@
-// Beverage Fetch code from origin
+var buttonEl = document.getElementById("");
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "e42bdfecf5mshf3552e7e1249ca6p16c62fjsn997c4e790fc4",
-    "X-RapidAPI-Host": "cocktail-by-api-ninjas.p.rapidapi.com",
-  },
-};
+// Beverage API permissions
+const bevOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "e42bdfecf5mshf3552e7e1249ca6p16c62fjsn997c4e790fc4",
+      "X-RapidAPI-Host": "cocktail-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+
+// Recipe API permissions
+const recipeOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "cbfed17221msh2efdb0eadae0eefp18de4bjsn3cd21032b10f",
+      "X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+
+
+// Search function
+var userSearch = function (event) {
+    event.preventDefault();
+    var searchInput = document.getElementById("FORM INPUT FIELD ID").value;
+    console.log(searchInput);
+    beverageSearch(searchInput);
+  };
+
+
+// Beverage Fetch code from origin
 
 fetch(
   "https://cocktail-by-api-ninjas.p.rapidapi.com/v1/cocktail?name=bloody%20mary",
-  options
+  bevOptions
 )
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
+
 
 // Recipe Fetch code from origin
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "cbfed17221msh2efdb0eadae0eefp18de4bjsn3cd21032b10f",
-    "X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com",
-  },
-};
 fetch(
   "https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=italian%20wedding%20soup",
-  options
+  recipeOptions
 )
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
+
+
+
+buttonEl.addEventListener("click", userSearch);
